07ad816ceb30295d51e2318b245f2cc03a969583
Arafat <rafuc25@gmail.com>
2026-09-10 14:25:30 +0500

    lab01: generated Spring Boot project skeleton

---
+++ src/test/java/kz/iitu/springab/SpringLab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.springab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
